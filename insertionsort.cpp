f2713fb81260a79100ac1b5a5db378b51ad4b597
Henzo <henzofelipe1@gmail.com>
2024-05-12 18:12:18 -0300

    Added insertionsort.cpp and updated makefile

---
+++ insertionsort.cpp
@@ -0,0 +1,39 @@
+#include "linkedlists.h"
+
+void vInsertionSort(Node*& ptrList)
+{
+    // Caso seja vazia, não há nada a fazer
+    if (ptrList == nullptr)
+        return;
+
+    // Ponteiros que percorrerão a lista
+    Node* ptrFoo = ptrList;
+    Node* ptrElem = ptrFoo;
+
+    // Enquanto a lista não for toda percorrida
+    while(ptrFoo != nullptr)
+    {
+        ptrElem = ptrFoo;
+
+        // Vai inserindo o novo elemento em sua posição na lista já parcialmente ordenada
+        while(ptrElem->ptrLast != nullptr && ptrElem->ptrLast->iValue > ptrElem->iValue)
+        {
+            // Troca os elementos e move o ponteiro para trás
+            vSwapElements(ptrElem, ptrElem->ptrLast);
+            ptrElem = ptrElem->ptrLast;
+        }
+
+        // Avança somente quando todos até ptrFoo estiverem em ordem
+        ptrFoo = ptrFoo->ptrNext;
+    }
+
+    return;
+}
+
+int main(void)
+{
+    // Faz 100 ordenações com listas de tamanho 10000
+    vRandomTests(100, 10000, vInsertionSort);
+
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,8 +1,9 @@
 all: clean
 
-test: bubblesort selectsort
+test: bubblesort selectsort insertionsort
 		bubblesort > bubbleTime.txt
 		selectsort > selectTime.txt
+		insertionsort > insertionTime.txt
 
 bubblesort: linkedlists.o bubblesort.cpp
 		g++ linkedlists.o bubblesort.cpp -o bubblesort
@@ -10,8 +11,11 @@ bubblesort: linkedlists.o bubblesort.cpp
 selectsort: linkedlists.o selectsort.cpp
 		g++ linkedlists.o selectsort.cpp -o selectsort
 
+insertionsort: linkedlists.o insertionsort.cpp
+		g++ linkedlists.o insertionsort.cpp -o insertionsort
+
 linkedlists.o: linkedlists.cpp linkedlists.h
 		g++ linkedlists.cpp -c
 
 clean:
-		rm -f linkedlists.o
+		del bubblesort.exe selectsort.exe insertionsort.exe linkedlists.o
