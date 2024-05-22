95e6b6e1262836c3984da32c416d1c22d23b8fe6
Henzo <henzofelipe1@gmail.com>
2024-05-21 22:25:22 -0300

    Finished counting sort

--- countingsort.cpp
+++ countingsort.cpp
@@ -31,40 +31,97 @@ void vCountingSort(Node*& ptrList)
         arriCounter[iElemValue] += 1;
     }
 
-    // Torna arriCounter acumulado
-    for (int j = 1; j < iMaxElem + 1; j++)
-        arriCounter[j] += arriCounter[j-1];
+    // Cria uma nova lista e insere os elementos já em ordem
+    Node* ptrSortedList = ptrCreateList();
 
-    // Cria um novo array, dessa vez ordenado
-    int arriSorted[iSize] = {};
-
-    for (int k = iSize - 1; k > -1; k--)
+    for (int k = iMaxElem; k != -1; k--)
     {
-        // Acha o elemento naquela posição
-        int iElemValue = iFindElement(ptrList, k);
-
-        // Adiciona ordenado e subtrai de arriCounter
-        arriCounter[iElemValue] -= 1;
-        arriSorted[arriCounter[iElemValue]] = iElemValue;
+        // Adiciona-o todas vezes que foram contadas
+        for (int i = arriCounter[k]; i > 0; i--)
+        {
+            vAddElemFront(ptrSortedList, k);
+        }
     }
 
-    // Converte para listas encadeadas :)
-    Node* ptrSorted = ptrConvertArrayList(arriSorted, iSize);
-
+    // Apaga a anterior e aponta para a nova
     vDeleteList(ptrList);
-    ptrList = ptrSorted;
+    ptrList = ptrSortedList;
 }
+
+void vShowCountingSort(Node*& ptrList)
+{
+    // Apresenta a lista desordenada
+    cout << "A lista original: ";
+    vPrintList(ptrList);
+
+    Node* ptrFoo = ptrList;
+    int iSize = 0;
+    int iMaxElem = 0;
+
+    // Acha o maior valor de ptrList e seu tamanho
+    for (; ptrFoo != nullptr; iSize++)
+    {
+        // Caso for maior, atualiza
+        if (ptrFoo->iValue > iMaxElem)
+            iMaxElem = ptrFoo->iValue;
+
+        // Avança
+        ptrFoo = ptrFoo->ptrNext;
+    }
+
+    // Mostra o maior elemento
+    cout << "O maior elemento: " << iMaxElem << endl;
+
+    // Cria um array zerado para contar cada elemento da lista
+    // (Para que haja acesso imediato)
+    int arriCounter[iMaxElem + 1] = {};
+
+    // Conta todos elementos de ptrList
+    for (int i = 0; i < iSize; i++)
+    {
+        // Acha o elemento naquela posição
+        int iElemValue = iFindElement(ptrList, i);
+
+        // E conta sua frequência
+        arriCounter[iElemValue] += 1;
+    }
+
+    // Mostra a contagem
+    cout << "A contagem de cada elemento: ";
+    
+    for (int i = 0; i != iMaxElem + 1; i++)
+        cout << arriCounter[i] << " ";
+
+    cout << endl;
+
+    // Cria uma nova lista e insere os elementos já em ordem
+    Node* ptrSortedList = ptrCreateList();
+
+    for (int k = iMaxElem; k != -1; k--)
+    {
+        // Adiciona-o todas vezes que foram contadas
+        for (int i = arriCounter[k]; i > 0; i--)
+        {
+            vAddElemFront(ptrSortedList, k);
+        }
+    }
+
+    // Apaga a anterior e aponta para a nova
+    vDeleteList(ptrList);
+    ptrList = ptrSortedList;
+
+    // Mostra a nova lista
+    cout << "Entao recriamos a lista em ordem: ";
+    vPrintList(ptrList);
+}
+
 
 int main(void)
 {
     Node* ptrList = ptrGenerateRandomList(5);
-    cout << "A lista original: ";
-    vPrintList(ptrList);
 
-    cout << "A lista ordenada: ";
-    vCountingSort(ptrList);
-    vPrintList(ptrList);
-
+    // Faz o sort, mostrando cada etapa
+    vShowCountingSort(ptrList);
 
     return 0;
 }
